80530a0d372b0728f0bb85daf3308df092e845b7
corifowler <fowler.cori@gmail.com>
2017-04-07 15:40:30 -0400

    added revised changes

--- src/pages/billing-history-info/billing-history-info.ts
+++ src/pages/billing-history-info/billing-history-info.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { DashboardPage } from '../dashboard/dashboard';
 
 @Component({
-  selector: 'page-pay-confirmation',
-  templateUrl: 'pay-confirmation.html',
+  selector: 'billing-history-info',
+  templateUrl: 'billing-history-info.html',
 })
-export class PayConfirmationPage {
+export class BillingHistoryInfoPage {
       private dashboardPage;
 
       constructor(public navCtrl: NavController, public navParams: NavParams) {
-          this.dashboardPage = DashboardPage;
-      }
-
-      private returnToDashboard() {
-          this.navCtrl.setRoot(this.dashboardPage);
       }
 
       ionViewDidLoad() {
--- src/pages/billing-history/billing-history.ts
+++ src/pages/billing-history/billing-history.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { BillingHistoryInfoPage } from '../billing-history-info/billing-history-info';
 
 @Component({
   selector: 'page-billing-history',
   templateUrl: 'billing-history.html',
 })
 export class BillingHistoryPage {
+    private billingHistoryInfoPage;
     private billingHistory: Array<any> = [
         {
             date: 'March 2017',
@@ -46,6 +48,7 @@ export class BillingHistoryPage {
     ];
 
     constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.billingHistoryInfoPage = BillingHistoryInfoPage;
     }
 
     ionViewDidLoad() {
